c886cc360a3a4a98f5babdb65b0e18076f737a61
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-04-26 13:42:55 +0600

    status changer functions and jquery added

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -1,3 +1,8 @@
+$(async function () {
+    await loadTasks();
+    await addTask();
+});
+
 const baseUrl = 'http://localhost:8080';
 
 async function getTasks() {
@@ -9,26 +14,29 @@ async function getTasks() {
     }
 }
 async function loadTasks() {
-    const tasks = getTasks();
-    await tasks.then(taskData => {
+    const tasks = await getTasks();
+    tasks.content.forEach(task => {
 
-        taskData.content.forEach((task) => {
-
-            console.log(!document.getElementById(task.id));
-            !document.getElementById(task.id)&&addTaskElement(createTaskElement(task));
-            // addTaskElement(createTaskElement(task));
-        });
+        !document.getElementById(task.id)&&
+        addTaskElement(createTaskElement(task));
     });
+    await complete();
 }
-loadTasks();
+
 const taskForm = document.getElementsByClassName("task-form")[0];
-taskForm.addEventListener('submit', submitHandler);
-function submitHandler(e) {
+async function submitHandler(e) {
     e.preventDefault();
     const form = e.target;
     let data = new FormData(form);
-    createTask(data);
-    loadTasks();
+    await createTask(data);
+    await loadTasks();
+    // await complete();
+
+    taskForm.reset();
+    taskForm.firstElementChild.focus();
+}
+async function addTask(){
+    await taskForm.addEventListener('submit', submitHandler);
 }
 async function createTask(taskFormData) {
 
@@ -47,13 +55,54 @@ async function createTask(taskFormData) {
     console.log(responseData);
 }
 function addTaskElement(taskElement) {
-    document.getElementsByClassName('task-list')[0].append(taskElement)
+    document.getElementsByClassName('task-list')[0].append(taskElement);
 }
 function createTaskElement(taskObject) {
-    let taskList = document.createElement('li')
+    let taskList = document.createElement('li');
     taskList.id = `${taskObject.id}`;
     taskList.innerHTML =
-        `<p>${taskObject.description}</p>`;
-        // `<p >${taskObject.dateTime}</p>`;
+        `<p class="${taskObject.done}">${taskObject.description}</p>`;
+    taskList.style.userSelect = 'none';
+    if(taskList.firstElementChild.classList.contains("true")){
+        taskList.firstElementChild.style.textDecoration = 'line-through';
+    }
     return taskList;
+}
+
+async function complete() {
+
+    for (let i = 0; i < document.getElementsByTagName('p').length; i++){
+        let taskList = document.getElementsByTagName('p')[i];
+        await taskList.addEventListener('dblclick',  async function (e) {
+
+            const taskJSON = JSON.stringify(({id: taskList.parentElement.id}));
+            const settings = {
+                method: 'PUT',
+                cache: 'no-cache',
+                mode : 'cors',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: taskJSON
+            };
+            await fetch(baseUrl + '/tasks', settings);
+            // await loadTasks();
+            console.log(taskList.style.textDecoration);
+            if(taskList.style.textDecoration === 'line-through'){
+                unMark(e)
+            }else{
+                mark(e);
+            }
+        });
+    }
+}
+function mark(e) {
+    console.log('mark');
+    const list = e.target;
+    list.style.textDecoration = 'line-through';
+}
+function unMark(e) {
+    console.log('unmark');
+    const list = e.target;
+    list.style.textDecoration = 'none currentcolor solid';
 }
